512c2b89880d37156c2b71ac9c4e41e948304bd1
Tam Pham <tampham47@live.com>
2023-04-29 14:10:16 +0700

    chore: yarn add gatsby-plugin-netlify

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -13,6 +13,7 @@ const config: GatsbyConfig = {
   // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
   plugins: [
+    "gatsby-plugin-netlify",
     {
       resolve: "gatsby-source-notion-api",
       options: {
